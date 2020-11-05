9ffd4c29f210e5af670b937e83b8c518e79e0f4a
Sam Dillard <sam@influxdata.com>
2020-11-04 17:54:08 -0800

    add ability to 'hold' keys in place for a single batch -- persists keys per line in batch

--- primitives.py
+++ primitives.py
@@ -13,32 +13,56 @@ def _gen_int(num):
     upper_bound = 10 ** num - 1
     return(random.randint(lower_bound,upper_bound))
 
-def gen_tag(tag_key_size, tag_value_size):
+def gen_tag(tag_key_size, tag_value_size, key=''):
     # return string Tag key-value pair
-    key = 'tag_' + _gen_string((tag_key_size-4))
-    val = _gen_string(tag_value_size)
-    pair = f",{key}={val}"
+    if key:
+        held_key = 'tag_' + key
+        held_key = held_key[:-4]
+        val = _gen_string(tag_value_size)
+        pair = f",{held_key}={val}"
+    else:
+        key = 'tag_' + _gen_string((tag_key_size-4))
+        val = _gen_string(tag_value_size)
+        pair = f",{key}={val}"
     return(pair)
 
-def gen_str_field(field_key_size, field_value_size):
+def gen_str_field(field_key_size, field_value_size, key=''):
     # return string Tag key-value pair
-    key = 'str_' + _gen_string(field_key_size-4)
-    val = _gen_string(field_value_size)
-    pair = f",{key}='{val}'"
+    if key:
+        held_key = 'str_' + key
+        held_key = held_key[:-4]
+        val = _gen_string(field_key_size)
+        pair = f",{held_key}={val}"
+    else:
+        key = 'str_' + _gen_string(field_key_size-4)
+        val = _gen_string(field_value_size)
+        pair = f",{key}='{val}'"
     return(pair)
 
-def gen_int_field(field_key_size, int_value_size):
+def gen_int_field(field_key_size, int_value_size, key=''):
     # return int Field key-value pair
-    key = 'int_' + _gen_string(field_key_size-4)
-    val = _gen_int(int_value_size)
-    pair = f",{key}={val}i"
+    if key:
+        held_key = 'int_' + key
+        held_key = held_key[:-4]
+        val = _gen_int(int_value_size)
+        pair = f",{held_key}={val}"
+    else:
+        key = 'int_' + _gen_string(field_key_size-4)
+        val = _gen_int(int_value_size)
+        pair = f",{key}={val}i"
     return(pair)
 
-def gen_float_field(field_key_size, float_value_size):
+def gen_float_field(field_key_size, float_value_size, key=''):
     # return float Field key-value pair
-    key = 'fl_' + _gen_string(field_key_size-3)
-    val = round(random.uniform(10,99), float_value_size-2)
-    pair = f",{key}={val}"
+    if key:
+        held_key = 'fl_' + key
+        held_key = held_key[:-3]
+        val = _gen_int(float_value_size)
+        pair = f",{held_key}={val}"
+    else:
+        key = 'fl_' + _gen_string(field_key_size-3)
+        val = round(random.uniform(10,99), float_value_size-2)
+        pair = f",{key}={val}"
     return(pair)
 
 def gen_ts(precision = 's'):
--- sets.py
+++ sets.py
@@ -4,35 +4,52 @@ import time
 import primitives
 
 # Return set of Tag key-value pairs with trailing space
-def gen_tagset(num_tags, tag_key_size, tag_value_size):
-    tagset = ''.join(primitives.gen_tag(tag_key_size,tag_value_size) for i in range(num_tags)) + ' '
+def gen_tagset(num_tags, tag_key_size, tag_value_size, tag_keys):
+    if tag_keys:
+        tagset = ''.join(primitives.gen_tag(tag_key_size,tag_value_size, key=i) for i in tag_keys) + ' ' 
+    else:
+        tagset = ''.join(primitives.gen_tag(tag_key_size,tag_value_size) for i in range(num_tags)) + ' '
+    
     return(tagset[1:]) # remove leading comma
 
-
 # Following 3 functions are helper functions for `gen_fieldset()`
-def _gen_int_fieldset(int_fields, field_key_size, int_value_size):
-    int_fieldset = ''.join(primitives.gen_int_field(field_key_size,int_value_size) for i in range(int_fields))
+def _gen_int_fieldset(int_fields, field_key_size, int_value_size, int_field_keys):
+    if int_field_keys:
+        int_fieldset = ''.join(primitives.gen_int_field(field_key_size,int_value_size, key=i) for i in int_field_keys)       
+    else:
+        int_fieldset = ''.join(primitives.gen_int_field(field_key_size,int_value_size) for i in range(int_fields))
+
     return(int_fieldset[1:])
 
-def _gen_float_fieldset(float_fields, field_key_size, float_value_size):
-    float_fieldset = ''.join(primitives.gen_float_field(field_key_size,float_value_size) for i in range(float_fields))
+def _gen_float_fieldset(float_fields, field_key_size, float_value_size, float_field_keys):
+    if float_field_keys:
+        float_fieldset = ''.join(primitives.gen_float_field(field_key_size,float_value_size, key=i) for i in float_field_keys) 
+    else:
+        float_fieldset = ''.join(primitives.gen_float_field(field_key_size,float_value_size) for i in range(float_fields))
+    
     return(float_fieldset[1:])
 
-def _gen_str_fieldset(str_fields, field_key_size, str_value_size):
-    str_fieldset = ''.join(primitives.gen_str_field(field_key_size,str_value_size) for i in range(str_fields))
+def _gen_str_fieldset(str_fields, field_key_size, str_value_size, str_field_keys):
+    if str_field_keys:
+        str_fieldset = ''.join(primitives.gen_str_field(field_key_size,str_value_size, key=i) for i in str_field_keys)
+    else:
+        str_fieldset = ''.join(primitives.gen_str_field(field_key_size,str_value_size) for i in range(str_fields))
     return(str_fieldset[1:])
 
 # Use fieldset helper functions to generate a full fieldset
-def gen_fieldset(int_fields=2, 
-                float_fields=1, 
-                str_fields=1, 
-                field_key_size=8,
-                int_value_size=4,
-                float_value_size=4,
-                str_value_size=8):
-    int_fieldset = _gen_int_fieldset(int_fields, field_key_size, int_value_size)
-    float_fieldset = _gen_float_fieldset(float_fields, field_key_size, float_value_size)
-    str_fieldset = _gen_str_fieldset(str_fields, field_key_size, str_value_size)
+def gen_fieldset(int_fields, 
+                float_fields, 
+                str_fields, 
+                field_key_size,
+                int_value_size,
+                float_value_size,
+                str_value_size,
+                int_field_keys,
+                float_field_keys,
+                str_field_keys):
+    int_fieldset = _gen_int_fieldset(int_fields, field_key_size, int_value_size, int_field_keys)
+    float_fieldset = _gen_float_fieldset(float_fields, field_key_size, float_value_size, float_field_keys)
+    str_fieldset = _gen_str_fieldset(str_fields, field_key_size, str_value_size, str_field_keys)
 
     fieldset = ''
     if int_fieldset:
