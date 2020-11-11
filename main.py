6a110555079d9e8822e86dd27eb4146e96326394
Sam Dillard <sam@influxdata.com>
2020-11-10 17:04:47 -0800

    fix bug where hold_keys was not settable to False

--- main.py
+++ main.py
@@ -52,7 +52,10 @@ def gen_batch(measurement,
         str_field_keys   = [primitives._gen_string(tag_key_size) for i in range(str_fields)]
     else:
         tag_keys = []
-
+        int_field_keys = []
+        float_field_keys = []
+        str_field_keys = []
+        
     batch = []
     for i in range(batch_size):
         line = gen_line(measurement, 
@@ -75,6 +78,8 @@ def gen_batch(measurement,
 
     return(batch)
 
+import sys
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate a batch of Line Protocol points of a specified shape")
     parser.add_argument('measurement', type=str, default='cpu')
@@ -90,40 +95,27 @@ if __name__ == "__main__":
     parser.add_argument('--float_value_size', type=int, default=4)
     parser.add_argument('--str_value_size', type=int, default=8)
     parser.add_argument('--precision', type=str, choices = ['s','S','ms','MS','us','US','ns','NS'], default='s')
-    parser.add_argument('--hold_keys', type=bool, default=True)
+    parser.add_argument('--hold_keys', action='store_true')
+
+
+    print(f"raw args: {sys.argv}")
 
     args = parser.parse_args()
 
-    measurement = args.measurement
-    batch_size = args.batch_size
-    num_tags = args.num_tags
-    int_fields = args.int_fields
-    float_fields = args.float_fields
-    str_fields = args.str_fields
-    tag_key_size = args.tag_key_size
-    tag_value_size = args.tag_value_size
-    field_key_size = args.field_key_size
-    int_value_size = args.int_value_size
-    float_value_size = args.float_value_size
-    str_value_size = args.str_value_size
-    precision = args.precision
-    hold_keys = args.hold_keys
-
-    # print(args)
-    batch = gen_batch(measurement, 
-                batch_size,
-                num_tags,
-                int_fields,
-                float_fields,
-                str_fields,
-                tag_key_size,
-                tag_value_size,
-                field_key_size,
-                int_value_size,
-                float_value_size,
-                str_value_size,
-                precision,
-                hold_keys)
+    batch = gen_batch(args.measurement, 
+                args.batch_size,
+                args.num_tags,
+                args.int_fields,
+                args.float_fields,
+                args.str_fields,
+                args.tag_key_size,
+                args.tag_value_size,
+                args.field_key_size,
+                args.int_value_size,
+                args.float_value_size,
+                args.str_value_size,
+                args.precision,
+                args.hold_keys)
 
     for line in batch:
         print(line)
