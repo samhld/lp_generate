bbec057c7fabf213dc6a28f6a795d96609b6e290
Sam Dillard <sam@influxdata.com>
2021-04-01 15:09:42 -0700

    --keep_keys_session now holds keys constant for each batch; need to hold values now

--- main.py
+++ main.py
@@ -13,15 +13,17 @@ def gen_line(tag_keys, int_field_keys, float_field_keys, str_field_keys, **kwarg
 
     return(line)
 
-def gen_batch(tag_keys, int_field_keys, float_field_keys, str_field_keys, **kwargs):
+def gen_batch(kwargs, tag_keys=None, int_field_keys=None, float_field_keys=None, str_field_keys=None):
     
-    # Generate keys at batch level if `hold_keys` is True. 
+    # Generate keys at runtime level if `keep_keys_session` is True. 
     # This will keep Tag keys constant per line, reducing Series creation at database level.
     if kwargs['keep_keys_session']:
         tag_keys         = tag_keys
         int_field_keys   = int_field_keys
         float_field_keys = float_field_keys
         str_field_keys   = str_field_keys
+    # Generate keys at batch level if `keep_keys_batch` is True. 
+    # This will keep Tag keys constant per line, reducing Series creation at database level.
     elif kwargs['keep_keys_batch']:
         tag_keys         = [primitives._gen_string(kwargs['tag_key_size']) for i in range(kwargs['num_tags'])]
         int_field_keys   = [primitives._gen_string(kwargs['tag_key_size']) for i in range(kwargs['int_fields'])]
@@ -39,25 +41,6 @@ def gen_batch(tag_keys, int_field_keys, float_field_keys, str_field_keys, **kwar
         batch.append(line)
 
     return(batch)
-
-def print_batch(kwargs):
-
-    if kwargs['keep_keys_session']:
-        tag_keys         = [primitives._gen_string(kwargs['tag_key_size']) for i in range(kwargs['num_tags'])]
-        int_field_keys   = [primitives._gen_string(kwargs['tag_key_size']) for i in range(kwargs['int_fields'])]
-        float_field_keys = [primitives._gen_string(kwargs['tag_key_size']) for i in range(kwargs['float_fields'])]
-        str_field_keys   = [primitives._gen_string(kwargs['tag_key_size']) for i in range(kwargs['str_fields'])]
-        kwargs['keep_keys_batch']  = True
-    else:
-        tag_keys         = [] 
-        int_field_keys   = []
-        float_field_keys = []
-        str_field_keys   = []
-
-    batch = gen_batch(tag_keys, int_field_keys, float_field_keys, str_field_keys, **kwargs)
-
-    for line in batch:
-        print(line)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate a batch of Line Protocol points of a specified shape")
@@ -78,14 +61,31 @@ if __name__ == "__main__":
     parser.add_argument('--keep_keys_session', action='store_true', help="")
     parser.add_argument('--loop', action='store_true')
 
-
-
     kwargs = vars(parser.parse_args())
 
-    if kwargs['loop']:
-        while True:
-            print_batch(kwargs)
-            time.sleep(config.regular_sample)
-        
-    else:
-        print_batch(kwargs)
+    if kwargs["loop"]:
+
+        if kwargs['keep_keys_session']:
+            tag_keys         = [primitives._gen_string(kwargs['tag_key_size']) for i in range(kwargs['num_tags'])]
+            int_field_keys   = [primitives._gen_string(kwargs['field_key_size']) for i in range(kwargs['int_fields'])]
+            float_field_keys = [primitives._gen_string(kwargs['field_key_size']) for i in range(kwargs['float_fields'])]
+            str_field_keys   = [primitives._gen_string(kwargs['field_key_size']) for i in range(kwargs['str_fields'])]
+            kwargs['keep_keys_batch']  = True
+
+            while True:
+                print(kwargs["keep_keys_batch"])
+                batch = gen_batch(kwargs,
+                                  tag_keys=tag_keys, 
+                                  int_field_keys=int_field_keys, 
+                                  float_field_keys=float_field_keys, 
+                                  str_field_keys=str_field_keys)
+                
+                for line in batch:
+                    print(line)
+
+                time.sleep(config.regular_sample)
+        else:
+            batch = gen_batch(kwargs)
+
+            for line in batch:
+                print(line)
